81bd95d32ec10957cc46d0615cd355c0ca0f6a7a
Michael Geissler <7744975+toolsfactory@users.noreply.github.com>
2021-01-09 16:52:28 +0100

    Added unit conversion extensions

---
+++ src/Tiveria.Common/Extensions/UnitConversions/UnitConversionExtensions_Preassure.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiveria.Common.Extensions
+{
+    public static partial class UnitConversionExtensions
+    {
+
+        /// <summary>
+        /// Convert preassure in inch of mercury to millibar
+        /// </summary>
+        /// <param name="inHg">preassure in inch of mercury</param>
+        /// <returns>preassure in millibar</returns>
+        public static double InHg2mBar(this double inHg)
+        {
+            return (inHg * 33.864);
+        }
+
+        /// <summary>
+        /// Convert preassure in millibar to inch of mercury
+        /// </summary>
+        /// <param name="mBar">preassure in millibar</param>
+        /// <returns>preassure in inch of mercury</returns>
+        public static double MBar2InHg(this double mBar)
+        {
+            return (mBar / 33.864);
+        }
+
+        /// <summary>
+        /// Convert preassure in millibar to Hectopascal
+        /// </summary>
+        /// <param name="mBar">preassure in millibar</param>
+        /// <returns>hectopascal</returns>
+        public static double MBar2hPa(this double mBar)
+        {
+            return mBar;
+        }
+
+        /// <summary>
+        /// Convert preassure in millibar to (technical) Atmosphere
+        /// </summary>
+        /// <param name="mBar">preassure in millibar</param>
+        /// <returns>atmosphere</returns>
+        public static double MBar2AT(this double mBar)
+        {
+            return mBar * 0.0010197162129779282;
+        }
+
+        /// <summary>
+        /// Convert preassure in millibar to (standard) Atmosphere
+        /// </summary>
+        /// <param name="mBar">preassure in millibar</param>
+        /// <returns>atmosphere</returns>
+        public static double MBar2ATM(this double mBar)
+        {
+            return mBar * 0.00098692316931427;
+        }
+    }
+}
---
+++ src/Tiveria.Common/Extensions/UnitConversions/UnitConversionExtensions_Temperature.cs
@@ -0,0 +1,39 @@
+﻿namespace Tiveria.Common.Extensions
+{
+    /// <summary>Provides extension methods for trigonometric, logarithmic, and other common mathematical functions.</summary>
+    public static partial class UnitConversionExtensions
+    {
+
+        #region Unit Conversions
+        public static double Fahrenheit2Celsius(this double fahrenheit)
+        {
+            return ((fahrenheit - 32) * 5) / 9;
+        }
+
+        public static double Celsius2Fahrenheit(this double celcius)
+        {
+            return (celcius * 9) / 5 + 32;
+        }
+
+        public static double Mile2KM(this double mile)
+        {
+            return (mile * 1.609344);
+        }
+
+        public static double KM2Mile(this double km)
+        {
+            return (km / 1.609344);
+        }
+
+        public static double Inch2MM(this double inch)
+        {
+            return (inch * 25.4);
+        }
+
+        public static double mm2Inch(this double mm)
+        {
+            return (mm / 25.4);
+        }
+        #endregion 
+    }
+}
