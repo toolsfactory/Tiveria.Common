2a033d52ac184c1ff7740753f80853a8408b6197
Michael Geissler <7744975+toolsfactory@users.noreply.github.com>
2022-01-04 22:35:33 +0100

    Fixed typo in classname

---
+++ src/Tiveria.Common/IO/BigEndianBinaryReader.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.IO;
+
+namespace Tiveria.Common.IO
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BigEndianBinaryReader : EndianBinaryReader
+    {
+        public BigEndianBinaryReader(Stream input) : base(input, Endian.Big)
+        {
+        }
+
+        public BigEndianBinaryReader(string file) : base(file, Endian.Big)
+        {
+        }
+
+        public BigEndianBinaryReader(byte[] bytes) : base(bytes, Endian.Big)
+        {
+        }
+
+        public BigEndianBinaryReader(Stream input, Encoding encoding) : base(input, encoding, Endian.Big)
+        {
+        }
+
+        public BigEndianBinaryReader(byte[] bytes, int offset) : base(bytes, offset, Endian.Big)
+        {
+        }
+
+        public BigEndianBinaryReader(byte[] bytes, int offset, int count) : base(bytes, offset, count, Endian.Big)
+        {
+        }
+    }
+}
